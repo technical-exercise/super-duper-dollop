a0558e229128616b529aba2b942bb45d7fab0811
Ivan <me@example.com>
2019-02-11 16:41:17 -0600

    Updates

---
+++ precache-manifest.0a3fe84d2f256baac8456aa6d345d15d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7843df93bf1c6bcd9348",
+    "url": "/super-duper-dollop/static/js/main.7843df93.chunk.js"
+  },
+  {
+    "revision": "f437e907ea9b84a23eeb",
+    "url": "/super-duper-dollop/static/js/runtime~main.f437e907.js"
+  },
+  {
+    "revision": "2c850604d42c6e56c185",
+    "url": "/super-duper-dollop/static/js/2.2c850604.chunk.js"
+  },
+  {
+    "revision": "bc93b57799a10feab22c11525fffb218",
+    "url": "/super-duper-dollop/index.html"
+  }
+];
